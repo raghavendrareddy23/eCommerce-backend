b6bab456183d17edfb5e3c9db66240b273d27937
raghavendrareddy23 <70099426+raghavendrareddy23@users.noreply.github.com>
2024-02-12 18:31:51 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -36,3 +36,30 @@ exports.userLogin = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+exports.adminLogin = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username }); 
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    if (user.role !== 'admin') {
+      return res.status(403).json({ message: "Unauthorized: Only admins can access" });
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+
+    res.status(200).json({ token, username });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
