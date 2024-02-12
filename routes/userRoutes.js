c8691f500c085187924e194f428ff44aa209f700
raghavendrareddy23 <70099426+raghavendrareddy23@users.noreply.github.com>
2024-02-12 18:33:38 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,6 @@ const userController = require('../controllers/userController');
 
 router.post('/signup', userController.userSignup);
 router.post('/login', userController.userLogin);
+router.post('/adminlogin', userController.adminLogin);
 
 module.exports = router;
