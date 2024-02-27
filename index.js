2f925db4bde515302187df4f55be24d21171e6bf
raghavendrareddy23 <70099426+raghavendrareddy23@users.noreply.github.com>
2024-02-27 18:02:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// const adminRoutes = require('./routes/adminRoutes');
 const userRoutes = require('./routes/userRoutes');
 const categoryRoutes = require('./routes/categoryRoutes')
 const subCategoryRoutes = require('./routes/subCategoryRoutes');
 const productRoutes = require('./routes/productRoutes');
-const cartRoutes = require('./routes/cartRoutes')
+const cartRoutes = require('./routes/cartRoutes');
+const wishListRoutes = require('./routes/wishListRoutes');
+const addressRoutes = require('./routes/addressRoutes')
 const dotenv = require("dotenv");
 const connectDB = require('./utils/db');
 const cors = require('cors');
@@ -16,13 +17,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Routes
-// app.use('/admin', adminRoutes);
 app.use('/user', userRoutes);
 app.use('/category', categoryRoutes);
 app.use('/subcategory', subCategoryRoutes);
 app.use('/product', productRoutes);
 app.use('/cart', cartRoutes);
+app.use('/wishlist', wishListRoutes);
+app.use('/address', addressRoutes);
 
 
 connectDB();
