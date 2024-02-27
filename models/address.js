8dc9a8822e8b9360299dcd0077b03eba0cc64988
raghavendrareddy23 <70099426+raghavendrareddy23@users.noreply.github.com>
2024-02-27 18:00:12 +0530

    added Models for wishlist and address

---
+++ models/address.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const addressSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ["home", "office", "other"],
+    default: "home",
+  },
+  fullName: {
+    type: String,
+    required: true,
+  },
+  addressLine1: {
+    type: String,
+    required: true,
+  },
+  addressLine2: String,
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  zip: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+});
+
+const Address = mongoose.model("Address", addressSchema);
+
+module.exports = Address;
---
+++ models/wishList.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const wishListSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    items: [{
+        productId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Product',
+            required: true
+        }
+    }]
+});
+
+const WishList = mongoose.model('WishList', wishListSchema);
+
+module.exports = WishList;
