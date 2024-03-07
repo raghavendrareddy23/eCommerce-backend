9ec22f2025d79714c0ce2b2b58f61dc49f6addfa
raghavendrareddy23 <70099426+raghavendrareddy23@users.noreply.github.com>
2024-03-07 09:00:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+// const adminRoutes = require('./routes/adminRoutes');
 const userRoutes = require('./routes/userRoutes');
 const categoryRoutes = require('./routes/categoryRoutes')
 const subCategoryRoutes = require('./routes/subCategoryRoutes');
 const productRoutes = require('./routes/productRoutes');
 const cartRoutes = require('./routes/cartRoutes');
 const wishListRoutes = require('./routes/wishListRoutes');
-const addressRoutes = require('./routes/addressRoutes')
+const addressRoutes = require('./routes/addressRoutes');
+const orderRoutes = require('./routes/orderRoutes');
 const dotenv = require("dotenv");
 const connectDB = require('./utils/db');
 const cors = require('cors');
@@ -17,6 +19,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Routes
+// app.use('/admin', adminRoutes);
 app.use('/user', userRoutes);
 app.use('/category', categoryRoutes);
 app.use('/subcategory', subCategoryRoutes);
@@ -24,6 +28,7 @@ app.use('/product', productRoutes);
 app.use('/cart', cartRoutes);
 app.use('/wishlist', wishListRoutes);
 app.use('/address', addressRoutes);
+app.use('/orders', orderRoutes);
 
 
 connectDB();
