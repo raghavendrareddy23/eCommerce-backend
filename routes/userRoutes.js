5f2f33bce2af0a5050ce6c324c7a0f33a51acec3
raghavendrareddy23 <70099426+raghavendrareddy23@users.noreply.github.com>
2024-03-07 08:55:13 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,7 @@ const userController = require('../controllers/userController');
 
 router.post('/signup', userController.signup);
 router.post('/login', userController.login);
+router.post('/forgot-password', userController.requestPasswordReset);
+router.post('/reset-password', userController.resetPassword);
 
 module.exports = router;
