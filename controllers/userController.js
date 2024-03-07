eee93b7225a74eb645155be4cb0f34bbb16763bc
raghavendrareddy23 <70099426+raghavendrareddy23@users.noreply.github.com>
2024-03-07 08:54:33 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const dotenv = require("dotenv");
+const {sendResetPasswordEmail, sendSuccessResetEmail} = require('../utils/resetpassword');
 
 exports.signup = async (req, res) => {
   try {
@@ -75,4 +76,71 @@ const adminLogin = async (req, res, user, password) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+
+exports.requestPasswordReset = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1m' }); 
+    user.resetPasswordToken = token;
+    user.resetPasswordExpires = Date.now() + 60000;
+
+    await user.save();
+
+    const resetLink = `http://localhost:5000/user/reset-password?token=${token}`;
+
+    await sendResetPasswordEmail(user.email, resetLink);
+
+    res.status(200).json({ message: "Reset password email sent successfully", resetLink , token});
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.resetPassword = async (req, res) => {
+  try {
+    const { token, newPassword, confirmPassword } = req.body;
+
+    if (newPassword !== confirmPassword) {
+      return res.status(400).json({ message: "New password and confirm password do not match" });
+    }
+
+    const user = await User.findOne({
+      resetPasswordToken: token,
+      resetPasswordExpires: { $gt: Date.now() } 
+    });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid or expired token" });
+    }
+
+    const isSameAsOldPassword = await bcrypt.compare(newPassword, user.password);
+    if (isSameAsOldPassword) {
+      return res.status(400).json({ message: "New password cannot be the same as the old password" });
+    }
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+    user.password = hashedPassword;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpires = undefined;
+
+    await user.save();
+
+    await sendSuccessResetEmail(user.email, 'Password reset successfully');
+
+    res.status(200).json({ message: "Password reset successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+
 
