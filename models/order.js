a474715f870ae495f1bb2efe680f32b39485b7c9
raghavendrareddy23 <70099426+raghavendrareddy23@users.noreply.github.com>
2024-04-02 08:38:24 +0530

    Update order.js

--- models/order.js
+++ models/order.js
@@ -1,29 +1,36 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const OrderSchema = new mongoose.Schema({
-    userId: {
+  userId: {
+    type: String,
+    ref: "User",
+  },
+  items: [
+    {
+      productId: {
         type: String,
-        ref: "User"
-    },
-    items: [{
-        productId: {
-            type: String,
-            ref: "Product"
-        },
-        quantity: {
-            type: Number,
-            required: true,
-            min: [1, 'Quantity cannot be less than 1.']
-        },
-    }],
-    totalBill: {
+        ref: "Product",
+      },
+      quantity: {
         type: Number,
-        default: 0
+        required: true,
+        min: [1, "Quantity cannot be less than 1."],
+      },
     },
-    date_added: {
-        type: Date,
-        default: Date.now
-    }
+  ],
+  addressId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Address",
+    required: true,
+  },
+  totalBill: {
+    type: Number,
+    default: 0,
+  },
+  date_added: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = Order = mongoose.model('Order', OrderSchema);
+module.exports = Order = mongoose.model("Order", OrderSchema);
