2569f54b86e772b18d94afb4d2e748fc04bb0088
raghavendrareddy23 <70099426+raghavendrareddy23@users.noreply.github.com>
2024-04-12 12:35:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// const adminRoutes = require('./routes/adminRoutes');
+const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
-const categoryRoutes = require('./routes/categoryRoutes')
+const categoryRoutes = require('./routes/categoryRoutes');
 const subCategoryRoutes = require('./routes/subCategoryRoutes');
 const productRoutes = require('./routes/productRoutes');
 const cartRoutes = require('./routes/cartRoutes');
 const wishListRoutes = require('./routes/wishListRoutes');
 const addressRoutes = require('./routes/addressRoutes');
 const orderRoutes = require('./routes/orderRoutes');
+const orderController = require('./controllers/orderController');
 const couponRoutes = require('./routes/couponRoutes');
 const dotenv = require("dotenv");
 const connectDB = require('./utils/db');
 const cors = require('cors');
+const cron = require('cron'); 
 
 const app = express();
 
 // Middleware
-app.use(express.json());
+app.use(express.json({
+    verify: (req, res, buf) => {
+        if (req.originalUrl.startsWith('/orders/webhook/stripe')) {
+            req.rawBody = buf.toString();
+        }
+    },
+}));
 app.use(cors());
 
-// Routes
-// app.use('/admin', adminRoutes);
 app.use('/user', userRoutes);
 app.use('/category', categoryRoutes);
 app.use('/subcategory', subCategoryRoutes);
@@ -31,13 +37,15 @@ app.use('/wishlist', wishListRoutes);
 app.use('/address', addressRoutes);
 app.use('/orders', orderRoutes);
 app.use('/coupon', couponRoutes);
-
+app.post('/orders/webhook/stripe', orderController.webhook);
 
 connectDB();
 
-// Start server
+const job = new cron.CronJob('*/12 * * * *', () => {
+    console.log('Cron job is running every 12 minutes in India...');
+}, null, true, 'Asia/Kolkata'); 
+
+job.start();
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-const cronJob = require('./utils/cronJob');
-cronJob.start();
